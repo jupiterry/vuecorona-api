abdef5b2646b9fbd30f18ff4bb0319a8645e0547
emirhan <jupiterryhd@gmail.com>
2021-04-25 18:53:10 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 // vue.config.js file to be place in the root of your repository
 
 module.exports = {
+  
     publicPath: process.env.NODE_ENV === 'production'
       ? '/vuecorona-api/'
       : '/'
   }
+ module.exports = {
+    css: {
+      extract: false
+    }
+  };
